2ada98b1c083b3dd8059cef8f68473ce1fffcf84
wei <u0151051@gmail.com>
2019-05-18 16:57:54 +0800

    Init

--- app.py
+++ app.py
@@ -17,6 +17,15 @@ line_bot_api = LineBotApi('58xsi39XnfzoMg+F45Et9YImICtIsCQ4q+XENgloI2lP6eX/C5UBv
 # Channel Secret
 handler = WebhookHandler('a47997ec5d862f32608fe9b4fb55271c')
 
+#抓垃圾車資訊
+def getrubbishtruck():
+    url="https://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json";
+    response=json.loads(requests.get(url).text);
+    content="";
+    for row in response:
+        content+="垃圾車:"+row["car"]+"-"+row["location"]+"\n";
+    return content;
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -38,12 +47,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    msg=event.message.text;
+    if "垃圾車" in msg:
+        txt=getrubbishtruck();
+    else:
+        txt=event.message.text;
+    message = TextSendMessage(text=txt)
     line_bot_api.reply_message(
         event.reply_token,
         message)
 
 import os
+import requests;
+import json;
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
