7dae4845c0c9a450c4593af80da0029eb17901ba
wei <u0151051@gmail.com>
2019-06-01 13:17:56 +0800

    Init

---
+++ inserttable.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 13:11:48 2019
+
+@author: User
+"""
+import psycopg2;
+conn=psycopg2.connect(database="ddr93dv9ort6eb",user="gxtkhrtqoowrme",password="c7602a59c8758ad0515037079e38be4e08cf7a7e44a42599e539359b2da9b9cb",host="ec2-54-225-106-93.compute-1.amazonaws.com",port="5432");
+insertsql="insert into  product(name,price)values('運動眼鏡',1000)";
+insertsql2="insert into  product(name,price)values('休閒眼鏡',800),('兒童眼鏡',300)";
+cur=conn.cursor();
+cur.execute(insertsql);
+cur.execute(insertsql2);
+conn.commit();
+conn.close();
