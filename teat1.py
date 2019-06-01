091746b35d924a11f9fc2e5dc21e6ec9b90b8a9d
wei <u0151051@gmail.com>
2019-06-01 12:30:56 +0800

    Init

--- teat1.py
+++ teat1.py
@@ -8,7 +8,8 @@ Created on Sat Jun  1 11:42:16 2019
 import psycopg2;
 conn=psycopg2.connect(database="ddr93dv9ort6eb",user="gxtkhrtqoowrme",password="c7602a59c8758ad0515037079e38be4e08cf7a7e44a42599e539359b2da9b9cb",host="ec2-54-225-106-93.compute-1.amazonaws.com",port="5432");
 createsql="create table product(id serial primary key,name varchar(50),price int)";
-cur=conn.cursor(createsql);
-conn.commit();
+cur=conn.cursor();
+cur.execute(createsql);
+cur.commit();
 conn.close();
 print("連線成功喔");
