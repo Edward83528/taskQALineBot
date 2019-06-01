939bcab57c351b05f1472f3ec34390ab9eac8910
wei <u0151051@gmail.com>
2019-06-01 15:07:47 +0800

    Init

--- function.py
+++ function.py
@@ -22,10 +22,10 @@ def getproduct(msg='1'):
     conn=psycopg2.connect(database="ddr93dv9ort6eb",user="gxtkhrtqoowrme",password="c7602a59c8758ad0515037079e38be4e08cf7a7e44a42599e539359b2da9b9cb",host="ec2-54-225-106-93.compute-1.amazonaws.com",port="5432");
     cur=conn.cursor();
     if msg==1:
-        print(msg);
+        print('a');
         cur.execute("select * from product");
     else:
-        print(msg);
+        print('b');
         cur.execute("select name,price from product where name like '%{}%' ".format(msg));
     rows=cur.fetchall();
     content='2';
