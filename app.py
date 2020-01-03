41dd79f1db911c2bd7a4b84b0530fede1b7218b1
wei0528 <u0151051@gmail.com>
2020-01-03 16:30:22 +0800

    Init

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, request, abort,render_template
-from nlp.olami import Olami #用威聖電子API
-import configparser #讀取設定檔
-#import pickle
+from flask import Flask, request, abort
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -13,22 +10,12 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
 
-#創建一個falsk對象
 app = Flask(__name__)
-#讀取設定檔
-config = configparser.ConfigParser()
-config.read('config.ini')
-# Channel Access Token
-line_bot_api = LineBotApi(config['Line']['token'])
-# Channel Secret
-handler = WebhookHandler(config['Line']['Channe_SECRET'])
-#載入分類模型(可替換)
-#clf = pickle.load(open("pkl/classifier.pkl","rb"))
 
-#跳轉首頁
-@app.route("/")
-def index():
-    return render_template("index.html")
+# Channel Access Token
+line_bot_api = LineBotApi('58xsi39XnfzoMg+F45Et9YImICtIsCQ4q+XENgloI2lP6eX/C5UBvycT9VuvPHFT5WdMTPQH3zdBbZp6XEfNig/xjm1f1gru62E/RkWqj19edVFazU8YYiu22P+6lJBXdGAKR0YAvoSvL9L9fCkr4QdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('a47997ec5d862f32608fe9b4fb55271c')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -48,60 +35,15 @@ def callback():
 
     return 'OK'
 
-glasses=0;
-air=0;
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    global glasses;
-    global air;
-    status=1;
-    msg=event.message.text;
-    
-    if "垃圾車" in msg:
-        txt=function.getrubbishtruck();
-    elif "蠟筆小新" in msg:
-        message=ImageSendMessage(original_content_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3fsAvfV91zePne1n1RzxuxhnKyQEXUSEVFvYvZAiHqzPJUhlIJQ&s',
-                                 preview_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3fsAvfV91zePne1n1RzxuxhnKyQEXUSEVFvYvZAiHqzPJUhlIJQ&s');
-        status=2;
-    elif "眼鏡" in msg:
-        glasses=1;
-        txt=function.getproduct();
-    elif "空氣" in msg:
-        air=1;
-        txt='請輸入一個地區名稱(例如:大里)';
-    elif "評論" in msg:
-        #txt=function.classify_review(msg,clf);
-        txt='評論';
-    elif "測試" in msg:
-        txt=function.gettest();
-    #填表之後要用圖表選單至能先用關鍵字測試
-    elif "填表" in msg:
-        txt=function.downdoc("fileTemplates/template.docx","fileOutput/test.docx");
-    else:
-        if glasses==1:
-            txt=function.getproduct(msg);
-            glasses=0;
-        elif air==1:
-            txt=function.getOpenData_pm25(msg);
-            air=0;
-        else:
-            #txt=function.getTextKey(msg); # 用字典的方式去抓關鍵字
-            txt=function.get_answer(msg) #用微軟qnamaker
-            if txt=='No good match found in KB.':
-                txt=Olami().nli(msg) #用威聖電子API
-        #txt=event.message.text;
-    if status==1:
-    
-        message = TextSendMessage(text=txt);
-
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         message)
 
-#引入自定義function
-import function;
 import os
-
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
